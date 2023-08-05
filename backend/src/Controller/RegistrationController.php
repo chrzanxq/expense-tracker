16e874dd573795db85100c5d70c7bda08a50d0d3
Chrzanxq <marcinch199@gmail.com>
2023-08-05 16:08:43 +0200

    registration process start

--- backend/src/Controller/RegistrationController.php
+++ backend/src/Controller/RegistrationController.php
@@ -8,11 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/registration', name: 'app_registration')]
-    public function index(): Response
+    #[Route('/register', name: 'register')]
+    public function registerForm(): Response
     {
-        return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
-        ]);
+        // Render the registration form template
+        return $this->render('registration/register.html.twig');
+    }
+
+    #[Route('/register', name: 'register_process', methods: ['POST'])]
+    public function registerProcess(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        // Process the submitted registration form data and create a new user
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        // Create a new User entity and set the email
+        $user = new User();
+        $user->setEmail($email);
+
+        // Encode the password before storing it in the database
+        $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+        $user->setPassword($encodedPassword);
+
+        // Save the user data to the database
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Redirect the user to a login page or dashboard after successful registration
+        return $this->redirectToRoute('login');
     }
 }
